5c525b3691ef4654a21f80c4deb70bef8009b7c0
leandrorangel94 <rangel94@outlook.com>
2021-06-08 14:59:02 -0300

    Data Annotations e AutoMapper

--- ProAgil.API/Controllers/EventoController.cs
+++ ProAgil.API/Controllers/EventoController.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProAgil.API.Dtos;
 using ProAgil.Domain;
 using ProAgil.Repository;
 
@@ -11,10 +14,12 @@ namespace ProAgil.API.Controllers
     public class EventoController : ControllerBase
     {
         private readonly IProAgilRepository _repo;
+        private readonly IMapper _mapper;
 
-        public EventoController(IProAgilRepository repo)
+        public EventoController(IProAgilRepository repo, IMapper mapper)
         {
             _repo = repo;
+            _mapper = mapper;
         }
 
         [HttpGet]
@@ -22,13 +27,15 @@ namespace ProAgil.API.Controllers
         {
             try
             {
-                var results = await _repo.GetAllEventoAsync(true);
+                var eventos = await _repo.GetAllEventoAsync(true);
+
+                var results = _mapper.Map<IEnumerable<EventoDto>>(eventos);
 
                 return Ok(results);
             }
-            catch (System.Exception)
+            catch (System.Exception ex)
             {
-                return this.StatusCode(StatusCodes.Status500InternalServerError, "Falha na conexão com banco de dados");
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Falha na conexão com banco de dados {ex.Message}");
             }
         }
 
@@ -37,7 +44,9 @@ namespace ProAgil.API.Controllers
         {
             try
             {
-                var results = await _repo.GetEventoAsyncById(EventoId, true);
+                var evento = await _repo.GetEventoAsyncById(EventoId, true);
+
+                var results = _mapper.Map<EventoDto>(evento);
 
                 return Ok(results);
             }
@@ -52,7 +61,9 @@ namespace ProAgil.API.Controllers
         {
             try
             {
-                var results = await _repo.GetAllEventoAsyncByTema(tema, true);
+                var eventos = await _repo.GetAllEventoAsyncByTema(tema, true);
+
+                var results = _mapper.Map<EventoDto[]>(eventos);
 
                 return Ok(results);
             }
@@ -63,15 +74,17 @@ namespace ProAgil.API.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post(Evento model)
+        public async Task<IActionResult> Post(EventoDto model)
         {
             try
             {
-                _repo.Add(model);
+                var evento = _mapper.Map<Evento>(model);
+
+                _repo.Add(evento);
 
                 if (await _repo.SaveChangesAsync())
                 {
-                    return Created($"api/evento/{model.Id}", model);
+                    return Created($"api/evento/{model.Id}", _mapper.Map<EventoDto>(evento));
                 }
             }
             catch (System.Exception)
@@ -83,18 +96,20 @@ namespace ProAgil.API.Controllers
         }
 
         [HttpPut("{EventoId}")]
-        public async Task<IActionResult> Put(int EventoId, Evento model)
+        public async Task<IActionResult> Put(int EventoId, EventoDto model)
         {
             try
             {
                 var evento = await _repo.GetEventoAsyncById(EventoId, false);
                 if (evento == null) return NotFound();
 
-                _repo.Update(model);
+                _mapper.Map(model, evento);
+
+                _repo.Update(evento);
 
                 if (await _repo.SaveChangesAsync())
                 {
-                    return Created($"api/evento/{model.Id}", model);
+                    return Created($"api/evento/{model.Id}", _mapper.Map<EventoDto>(evento));
                 }
             }
             catch (System.Exception)
---
+++ ProAgil.API/Dtos/EventoDto.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProAgil.API.Dtos
+{
+    public class EventoDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Local tem que ter entre 3 a 100 caracteres.")]
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+
+        [Required(ErrorMessage = "O Tema deve ser preenchido.")]
+
+        [Range(1, 120000, ErrorMessage = "A quantidade deve ser entre 1 a 120 mil pessoas.")]
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string ImageURL { get; set; }
+
+        [Phone]
+        public string Telefone { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+        public List<LoteDto> Lotes { get; set; }
+        public List<RedeSocialDto> RedesSociais { get; set; }
+        public List<PalestranteDto> Palestrantes { get; set; }
+    }
+}
